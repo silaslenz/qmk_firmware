4fa1a08be5fcbc8cbe94e0fed486466fcef589cc
Silas Lenz <silas.lenz@gmail.com>
2020-05-16 01:01:59 +0200

    Maybe fix configurator

--- keyboards/extreme_chopping/extreme_chopping.h
+++ keyboards/extreme_chopping/extreme_chopping.h
@@ -2,6 +2,10 @@
 
 #include "quantum.h"
 
-#ifdef KEYBOARD_extreme_cutting_rev1
+#ifdef KEYBOARD_lets_split_rev1
     #include "rev1.h"
+#elif KEYBOARD_lets_split_rev2
+    #include "rev2.h"
+#elif KEYBOARD_lets_split_sockets
+    #include "sockets.h"
 #endif
